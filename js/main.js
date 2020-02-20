53636e5cf0692f5e8e9a942910d7528cdc7f094b
Niccolò Predazzer <niccolo.predazzer@gmail.com>
2020-02-20 16:23:27 +0100

    Esercizio corretto

--- js/main.js
+++ js/main.js
@@ -7,12 +7,12 @@
 //faccio la stessa cosa ma quando il numero sarà divisibile per 3,5 o entrambi stamperò una cosa diversa usando if, if else else.
 
 for (var i = 1; i <= 100; i++) {
-    if (i % 3 == 0) {
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log('FizzBuzz');
+    } else if (i % 3 == 0)  {
         console.log('Fizz');
     } else if (i % 5 == 0) {
         console.log('Buzz');
-    } else if (i % 3 == 0 && i % 5 == 0) {
-        console.log('FizzBuzz');
     } else {
         console.log(i);
     }
